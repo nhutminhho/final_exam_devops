4f9d5b6f353e3533e723053a0c2947538e46246a
Ho Nhut Minh <nhutminhho@gmail.com>
2023-12-30 11:31:45 +0700

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+# import mysql.connector
+# import json
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  return "Welcome to Docker at Tel4vn"
+
+if __name__ == "__main__":
+  app.debug=True
+  app.run(host='0.0.0.0', port=5000)
